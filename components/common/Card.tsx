48c5706b31096dae2cd11fafe0ed298c14055aff
OnlineElite <online.0.elite@gmail.com>
2025-07-30 21:35:48 +0100

    Create a property folder

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -40,7 +40,7 @@ const Card : React.FC<PropertyProps> = ({name, address, rating, category, price,
         <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow duration-300 cursor-pointer">
             <div className="relative h-48 overflow-hidden">
                 <Image src={image}  alt={name} width={100} height={100}
-                className="w-full h-full object-cover hover:scale-105 transition-transform duration-300"/>
+                className="w-full h-full object-fill hover:scale-105 transition-transform duration-300"/>
             </div>
             <div className="p-4">
                 <h3 className="text-lg font-semibold text-gray-900 mb-2 line-clamp-1">{name}</h3>
